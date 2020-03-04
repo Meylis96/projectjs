cead6b74fccf2ca19b9e82d4792eb92921849b19
Meylis <meylis.muhamedov96@gmail.com>
2020-03-04 05:58:17 +0300

    fourth commit

--- script.js
+++ script.js
@@ -1,52 +1,71 @@
-'use strick';
+// 'use strick';
 
-var number = 5;
-var string = "Hello!";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
 
-let person = {
-    name: "John",
-    age: 23,
-    isMarried: false
+// let person = {
+//     name: "John",
+//     age: 23,
+//     isMarried: false
+// };
+// console.log(person.name);
+// console.log(person.age);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.png'];
+
+// console.log(arr[0]);
+
+// // alert("Hello World!");
+
+// // let answer = confirm("Are you here?")
+
+// // console.log(answer);
+
+
+// // let answer = +prompt("Are you 18?", "Yes");
+
+// // console.log(typeof(answer));
+
+// // console.log("arr" + " - object");
+// // console.log(4 + +" - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" == 2);
+
+// let isChecked = false,
+//     isClosed = false;
+
+// console.log(isChecked || !isClosed);
+
+
+let money = prompt("Ваш бюджет на месяц?"),
+    date = prompt("Введите дату в формате YYYY-MM-DD");
+console.log(money);
+console.log(date);    
+
+let appData = {
+    budget: 'money',
+    timeData: 'date',
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
-console.log(person.name);
-console.log(person.age);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.png'];
-
-console.log(arr[0]);
-
-// alert("Hello World!");
-
-// let answer = confirm("Are you here?")
-
-// console.log(answer);
+appData.money = date;
+console.log(appData);
 
 
-// let answer = +prompt("Are you 18?", "Yes");
 
-// console.log(typeof(answer));
-
-// console.log("arr" + " - object");
-// console.log(4 + +" - object");
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-console.log("2" == 2);
-
-let isChecked = false,
-    isClosed = false;
-
-console.log(isChecked || !isClosed);
-
-let sex = true;
-
+let answer = prompt("Введите обязательную статью расходов в этом месяце", "Во сколько обойдется?");
+console.log(answer);
