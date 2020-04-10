7b068c5f311dd8942408d16e9618cb72b1319ca8
Meylis <meylis.muhamedov96@gmail.com>
2020-04-10 22:20:18 +0300

    js.js changes

--- js.js
+++ js.js
@@ -1,21 +1,24 @@
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
-
-
-let a = prompt('Введите обязательную статью расходов в этом месяце'),
-    b = prompt('Во сколько обойдется?');
+let money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD');
 
 
 let appData = {
-    budget: 'money',
-    timeData: 'time',
-    expenses: {
-        a : b
-    },
+    budget: money, // No need to write 'money'
+    timeData: time, // No need to write 'time'
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-budget = alert(money / 30);
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''), //Don't forget about ..., ''
+    b2 = prompt('Во сколько обойдется?', ''),
+    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a4 = prompt('Во сколько обойдется?', '');
+    
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+// budget = alert(money / 30); Better to write like below
+alert(appData.budget / 30);
 
