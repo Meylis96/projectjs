96d735534a71f79479fd0b895bd81e14ce9edf0b
Meylis <meylis.muhamedov96@gmail.com>
2020-04-23 13:26:41 +0300

    JSON and AJAX lesson added

---
+++ Lesson_35_36/calc/script.js
@@ -0,0 +1,40 @@
+// JSON
+// let options = {
+//     width: 1366,
+//     height: 768,
+//     background: 'red',
+//     font: {
+//         size: '16px',
+//         color: '#fff'
+//     }
+// };
+
+// console.log(JSON.parse(JSON.stringify(options))); //parse to get the same view
+
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass);
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    //status of server
+    //statusText written status of server
+    //responseText written response of server /respone the same
+    //readyState
+
+    request.addEventListener('readystatechange', function(){
+        if(request.readyState === 4 && request.status === 200) {
+            let data = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Something is wrong!";
+        }
+    });
+});
