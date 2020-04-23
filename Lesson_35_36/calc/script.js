ebfea0516cbccd26d6b31ba2ba6d6154b1171b03
Meylis <meylis.muhamedov96@gmail.com>
2020-04-23 19:35:21 +0300

    hw added

--- Lesson_35_36/calc/script.js
+++ Lesson_35_36/calc/script.js
@@ -16,25 +16,38 @@ let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
-
-    // request.open(method, url, async, login, pass);
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-
-    //status of server
-    //statusText written status of server
-    //responseText written response of server /respone the same
-    //readyState
-
-    request.addEventListener('readystatechange', function(){
-        if(request.readyState === 4 && request.status === 200) {
-            let data = JSON.parse(request.response);
-
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Something is wrong!";
-        }
-    });
+    function catchData() {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            
+            // request.open(method, url, async, login, pass);
+            request.open('GET', 'current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+            
+            //status of server
+            //statusText written status of server
+            //responseText written response of server /respone the same
+            //readyState
+    
+            request.onload = function() {
+                if(request.readyState === 4) {
+                    if(request.status === 200) {
+                        resolve(this.response);
+                    } else {
+                        reject();
+                    }
+                }
+            };
+        });
+    }
+    catchData()
+    .then(response => {
+        console.log(response);
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
+    })
+    .then(() => console.log(5000))
+    .catch(() => inputUsd.value = "Что-то пошло не так")
+ 
 });
