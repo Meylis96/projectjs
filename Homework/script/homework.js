9fde9a9c6192c6a47dced253e0c6e2e5633bdd3b
Meylis <meylis.muhamedov96@gmail.com>
2020-04-24 21:31:29 +0300

    calculator added without bag

--- Homework/script/homework.js
+++ Homework/script/homework.js
@@ -345,6 +345,51 @@ window.addEventListener('DOMContentLoaded', function(){  //To load structure of 
             }
         }
     });
+
+    //Calculator
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function(){ // can't use () => because of this
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+            // bug
+        });
+
+        restDays.addEventListener('change', function(){ // can't use () => because of this
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(persons.value == '') {
+                totalValue.innerHTML = '0';
+            } else {
+                totalValue.innerHTML = total;
+            }
+            //bug
+        });
+
+        place.addEventListener('change', function() {
+            if(restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 });
 
 
