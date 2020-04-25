2d61b17ba3b41b97309cb2a3233eabcd4f475e68
Meylis <meylis.muhamedov96@gmail.com>
2020-04-25 17:27:19 +0300

    new code for calculator added

--- Homework/script/homework.js
+++ Homework/script/homework.js
@@ -348,46 +348,145 @@ window.addEventListener('DOMContentLoaded', function(){  //To load structure of 
 
     //Calculator
 
+    // let persons = document.querySelectorAll('.counter-block-input')[0],
+    //     restDays = document.querySelectorAll('.counter-block-input')[1],
+    //     place = document.getElementById('select'),
+    //     totalValue = document.getElementById('total'),
+    //     personsSum = 0,
+    //     daysSum = 0,
+    //     total = 0;
+
+    //     totalValue.innerHTML = 0;
+
+    //     persons.addEventListener('change', function(){ // can't use () => because of this
+    //         personsSum = +this.value;
+    //         total = (daysSum + personsSum) * 4000;
+
+    //         if(restDays.value == '') {
+    //             totalValue.innerHTML = 0;
+    //         } else {
+    //             totalValue.innerHTML = total;
+    //         }
+    //         // bug
+    //     });
+
+    //     restDays.addEventListener('change', function(){ // can't use () => because of this
+    //         daysSum = +this.value;
+    //         total = (daysSum + personsSum) * 4000;
+
+    //         if(persons.value == '') {
+    //             totalValue.innerHTML = '0';
+    //         } else {
+    //             totalValue.innerHTML = total;
+    //         }
+    //         //bug
+    //     });
+
+    //     place.addEventListener('change', function() {
+    //         if(restDays.value == '' || persons.value == '') {
+    //             totalValue.innerHTML = 0;
+    //         } else {
+    //             let a = total;
+    //             totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    //         }
+    //     });
+
     let persons = document.querySelectorAll('.counter-block-input')[0],
+
         restDays = document.querySelectorAll('.counter-block-input')[1],
+
         place = document.getElementById('select'),
+
         totalValue = document.getElementById('total'),
+
         personsSum = 0,
+
         daysSum = 0,
-        total = 0;
+
+        total = 0,
+
+        city = 0;
+
+
 
         totalValue.innerHTML = 0;
 
-        persons.addEventListener('change', function(){ // can't use () => because of this
+       
+
+        persons.addEventListener('input', function() {
+
             personsSum = +this.value;
-            total = (daysSum + personsSum) * 4000;
 
-            if(restDays.value == '') {
+
+
+            if(restDays.value == '' || personsSum == 0) {
+
                 totalValue.innerHTML = 0;
-            } else {
-                totalValue.innerHTML = total;
+
             }
-            // bug
+
+            else {
+
+                city = place.value;
+
+                total = ((daysSum + personsSum) * 4000) * city;
+
+                totalValue.innerHTML = total;
+
+            }
+
         });
 
-        restDays.addEventListener('change', function(){ // can't use () => because of this
-            daysSum = +this.value;
-            total = (daysSum + personsSum) * 4000;
 
-            if(persons.value == '') {
-                totalValue.innerHTML = '0';
-            } else {
-                totalValue.innerHTML = total;
+
+        restDays.addEventListener('input', function () {
+
+           daysSum = +this.value;
+
+           total = (daysSum + personsSum) * 4000;
+
+
+
+            if (persons.value == '' || daysSum == 0) {
+
+                totalValue.innerHTML = 0;
+
             }
-            //bug
+
+            else {
+
+                city = place.value;
+
+                total = ((daysSum + personsSum) * 4000) * city;
+
+                totalValue.innerHTML = total;
+
+            }
+
         });
+
+
 
         place.addEventListener('change', function() {
-            if(restDays.value == '' || persons.value == '') {
+
+            if (restDays.value == '' || persons.value == '') {
+
                 totalValue.innerHTML = 0;
-            } else {
-                let a = total;
-                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+
+            }
+
+            else {
+
+                personsSum = +persons.value;
+
+                daysSum = +restDays.value;
+
+                city = this.options[this.selectedIndex].value;
+
+                total = ((daysSum + personsSum) * 4000) * city;
+
+                totalValue.innerHTML = total;
+
             }
         });
 });
