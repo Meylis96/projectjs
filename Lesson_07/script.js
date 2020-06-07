d923512567f1212e1b22168e0174c3476a2966a2
Meylis <meylis.muhamedov96@gmail.com>
2020-06-07 18:53:20 +0300

    Lesson_07 added

---
+++ Lesson_07/script.js
@@ -0,0 +1,18 @@
+// alert("Hello"); // default by browser. it is not changable
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you older than 18?", ""); //"" don't forget
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old are you?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
+
+
