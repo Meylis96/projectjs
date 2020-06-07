efb89d4d17219138b2dadd8b0031946987df4048
Meylis <meylis.muhamedov96@gmail.com>
2020-06-07 15:56:27 +0300

    Lesson_06 added

---
+++ Lesson_06/script.js
@@ -0,0 +1,19 @@
+let number = 4;
+
+const person = "Alex";
+
+const bool = true; //or false
+
+let und;
+console.log(und);
+
+const object = {
+    name: 'Meylis',
+    age: 24,
+    isMarried: true
+};
+
+console.log(object["name"]); // it is better to write object.name
+
+let arr = ['plum.jpg', 'orange.png', 6, 'apple.jpeg', {}, []];
+console.log(arr[1]);
