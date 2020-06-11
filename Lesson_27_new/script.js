8258dfb3ec8e6bde62acb23d5f8b3cc450fc0cb6
Meylis <meylis.muhamedov96@gmail.com>
2020-06-11 13:33:03 +0300

    Lesson_27_new added

---
+++ Lesson_27_new/script.js
@@ -0,0 +1,11 @@
+5 // Because it return 5 and then after ++ it decreases
+(false) NaN - and it is String because empty array takes elems as string
+2 because it goes from right to left
+12 - and it is String because empty array takes elems as string
+1
+(true) null - && stamle on falsy and as null is falsy it returned its value
+false 1st Boolean 2nd is &&
+(false) 3 - because && is in higher priority and it returns 3 // || stumble on true
+No. different variables
+number because + turns to number
+2 because \\ return the first true value and it stops 
